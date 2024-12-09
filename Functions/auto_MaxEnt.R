2a41a9e94fc9687b4f4d57346050265cc2412c23
GonzaloAlbaladejo <gonalbala@gmail.com>
2024-12-09 13:16:13 +0000

    implementing general changes

--- Functions/auto_MaxEnt.R
+++ Functions/auto_MaxEnt.R
@@ -9,7 +9,7 @@ Auto_maxent<-function(
                       # Data preparation and variable selection
                       #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
                       presence_dat, # [sf/data.frame] sf object containing the points or data.frame with coordinates to the presence data
-                      predictors, # [RAST] rasterstack object with the environmental or predictor variables to use
+                      predictors, # [RAST] rasterstack object with the environmental or predictor variables to use (In all cases one object is needed). if `time_macth = TRUE` and `ignore_predictor = TRUE`, this spat_raster is used to define the study area resolution and the parameters for the time-series rasters
                       coords.p = c("decimalLongitude","decimalLatitude"), # [CHARACTER] if the spp_points is not a SpatialPoints object, provide the coordinates fields e.g x,y for ~x+y
                       min_obs = 10, # [NUMERICAL] Minimun number of observations to run the analysis
                       rm.dp = TRUE, # [LOGICAL] Should duplicated coordinates/points be removed from the presence data
@@ -18,12 +18,20 @@ Auto_maxent<-function(
                       crs.r = "EPSG:4326", # [CHARACTER] CRS for the spatial information `WGS84` is used by default
                       buff_lim = 0, # [NUMERIC] Buffer to apply to the study area (bounding box around the sp_range). This would depend on the units used for the CRS (degrees, meters, km, etc...)
                       n_bk = "AUTO", # [CHARACTER] Should the number of background points be established automatically
-                      prop_env = 0.25, # [NUMERIC] 0-1 If n_bk==AUTO, the proportion of environmental area to sample
+                      prop_env = NULL, # [NUMERIC] 0-1 If n_bk==AUTO, the proportion of environmental area to sample
                       type_bk = "Random", #[CHARACTER][Random,BwData,BwData_inv,EnvBK] Type of background sampling: Random = points are drawn at random from the study area; BwData = points are sampled from the study area based on a density kernell of the presence data; BwData_inv = points are sampled based on the inverse density kernell of the presence data; EnvBK = points are sampled based on a density kernel build using the first two Principal Components of a PCA build with the environmental data 
                       Test_n = 30, # [NUMERIC] values between 0-100, percentage of data separated for testing purposes
                       world_pol = NULL, # [sf] Sf polygon used to refine the study area. The main intention is to remove areas that might fall into the Ocean or to precisely delimit/remove other areas.
+                      
+                      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+                      # Some data coordination and selection
+                      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
                       time_macth = FALSE, # [LOGICAL] should we try to coordinate the variables time with the records dates? Still in the works, it would need more arguments in order to format all the dates/times formats
                       select_var = "NUMERICAL", # [CHARACTER] If `NUMERICAL` variables are selected based on VIF scores and correlation values (VIF<5 and COR<0.7). If `DIST` is used, the variables are selected based on the overlap of the density distributions of presence and absences [not implemented]
+                      d.range=NULL, # [NUMERIC] A vector of dates (years at the moment) to coordinate the point and variable data
+                      d.file=".tif$", # [CHARACTER] Format of the layers or variables to retrieve from the folders
+                      d.route, # "[CHARACTER] Route to the time-series environmental information. It can be more than one folder with multiple variables or time series. Apart from these at least a spat raster with the reference study area need to be provided in the predictors field
+                      ignore_predictor=FALSE, # [LOGICAL] Do we want to exclude the `predictor` data from the model or include its information (When we have temporal and fix variables, e.g Elevation)
                       
                       #~~~~~~~~~~~~~~~~~~#
                       # MaxEnt Modelling
@@ -56,10 +64,20 @@ Auto_maxent<-function(
   rm(list.of.packages,new.packages)
 
   # Preset some parameters
-  # a. Number of models to keep, it n.mods is set to be greather than n.m, set the first one to n.m
-  if(n.mods > n.m){
-    n.mods <- n.m
-  }
+  # a. Number of models and model parameters
+  if(mod.select==TRUE){
+      if(n.m==1 | n.m<=n.mods){
+        warning("Number of models to select equal or greather than models to run, check n.m and n.mods paramters! mod.select set to FALSE")
+        mod.select=FALSE
+        
+      }
+    
+    if(time_macth == FALSE){
+      warning("Model selection not available when models are fitted using time-series data!")
+      mod.select=FALSE
+    }
+    
+   }
   
   # 1. Format the presence data----
     if(nrow(presence_dat) < min_obs){
@@ -130,17 +148,28 @@ Auto_maxent<-function(
     pred.dat <- predictors %>% crop(sty.a %>% vect(),mask=TRUE)
 
     }else{
-    print("WARNING: predictors data is not an SpatRaster object!")
-    return(NULL)
+      print("WARNING: predictors data is not an SpatRaster object!")
+      return(NULL)
+    
     }
 
   # 2. Generate the background data ----
   # Get the number of background points
   if(n_bk=="AUTO"){
+    # With the AUTO parameter we are going to sample the study area until we find a number of background points that represents the
+    # distribution of variables of the study area (still optimizing this process!)
+    
+  if(is.null(prop_env)){
+    warning("No prop_env provided for the selection of background sample size. Setting the sample size to 10000, the MaxEnt default")
+      n_bk <- 10000 # use the MaxEnt Default method
+    }
+    
+  if(!is.null(prop_env)){
       # Get the XX proportion of cells from the study area
       n_bk <- prod(dim(pred.dat)[1:2])
       n_bk <- (n_bk * prop_env) %>% round(digits=0)
-
+  }
+    
     }else{
       if(!is.numeric(n_bk)){
         print("WARINING: bk_n field not_valid, setting parameter to `AUTO`")
@@ -152,6 +181,7 @@ Auto_maxent<-function(
       }
     }
 
+ 
   # 2.1 Create the background points ----
   if(type_bk %in% c("Random","BwData","BwData_inv")){
     bk_points <- backgroundPOINTS(presence = y_points,
@@ -159,22 +189,23 @@ Auto_maxent<-function(
                                   TrainTest = 1,
                                   range_samp = sty.a,
                                   weights.p = type_bk)$Train
-  }
+    }
   
   if(type_bk %in% "EnvBK"){
     # The selection of variables can have an impact on the shape and distribution of the PCA scores, therefore we are going to 
-    if(select_var!=FALSE){
+    if(select_var!=FALSE | time_macth == TRUE){
+      Warning("When Select_var is set to `TRUE` EnvBK is not available since the configuration of variables have an impact on the shape of the environmental hyperspace! Background points sampled at random!")
       bk_points <- backgroundPOINTS(presence = y_points,
                                     background_n = n_bk,
                                     TrainTest = 1,
                                     range_samp = sty.a,
                                     weights.p = "Random")$Train
       
-    }else{
+        }else{
       bk_points <-  bk_env(p.points=y_points,
-                         env_var=pred.dat,
-                         n_bk=n_bk,
-                         density_pc=TRUE)[["points"]] %>% st_cast("POINT")
+                          env_var=pred.dat,
+                          n_bk=n_bk,
+                          density_pc=TRUE)[["points"]] %>% st_cast("POINT")
       }
     }
 
@@ -182,22 +213,41 @@ Auto_maxent<-function(
   # Prepare the presence absence vector
   bk_points <- bk_points %>% mutate(presence=0,.before=0)
   y_points <- y_points %>% mutate(presence=1,.before=0)
-
+  
   obs_sp <- rbind(y_points %>% dplyr::select("presence"),
-            bk_points %>% dplyr::select("presence"))
-
+                  bk_points %>% dplyr::select("presence"))
+  
   obs_index <- obs_sp$presence %>% st_drop_geometry()
-
+  
+  
   # 3.a Extract the values from the environmental variables----
-  # Prepare the data.frame with the train and test data
   if(time_macth){
-      stop("Sorry, the `time_macth==TRUE` macthing tool is not implemented at this moment! but is something we are working on!")  
-      mod.dat <- pred.dat %>% terra::extract(obs_sp %>% vect(),ID=F)
+      warning("The `time_macth==TRUE` only works for years and only when the year is included in the name of the variable!")  
+      
+    mod.dat <- Time_machine(d.route=d.route,
+                          d.range=d.range,
+                          ref_rast=pred.dat)
+    
+    # Combine the two datasets and check their lengthgs
+    
+    
+    # Are there other variables (NEED TO RECONFIGURE PRED.DAT?)
+      
     
     }else{
+      
+      
       mod.dat <- pred.dat %>% terra::extract(obs_sp %>% vect(),ID=F)
-    
     }
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   # 3.a.1 Remove empty cases from the data----
    dat.index <- complete.cases(mod.dat)
@@ -257,6 +307,7 @@ Auto_maxent<-function(
     models <- list()
     mod.ARGS <- list()
   
+  # Set the different combinations of model adjustment paramters----
   if(random_features==FALSE){
     supress_features <- list()
     
@@ -277,45 +328,49 @@ Auto_maxent<-function(
   
   # 4.1 Run the different models----  
   set.seed(185)
-  for(w in 1:n.m){
-    
-    if(random_features==TRUE){ 
-      supress_features<-c("nolinear","noquadratic","noproduct","nothreshold")
-      xFeature<-sample(supress_features,size=sample(1:3,1),replace=F) # Select the number of predictive distributions to fit (this is random)
-      
-    }else{
-      xFeature<-supress_features[w,-c("beta.v")] %>% as.vector() %>% unlist() %>% unname()
-      xFeature <- xFeature[!is.na(xFeature)]
-    }
-    
-    if(is.null(beta.val)){
-    beta.m<-ifelse(random_features==TRUE,sample(1:15,1),supress_features[w,"beta.v"] %>% as.vector() %>% unlist() %>% unname())
-      }else{
-    beta.m<-supress_features[w,"beta.v"] %>% as.vector() %>% unlist() %>% unname()
-      }
+    for(w in 1:n.m){
+        
+      if(random_features==TRUE){ 
+          supress_features<-c("nolinear","noquadratic","noproduct","nothreshold")
+          xFeature <- sample(supress_features,size=sample(1:3,1),replace=F) # Select the number of predictive distributions to fit (this is random)
+          
+        }else{
+          xFeature<-supress_features[w,-c("beta.v")] %>% as.vector() %>% unlist() %>% unname()
+          xFeature <- xFeature[!is.na(xFeature)]
+        }
+        
+      if(is.null(beta.val)){
+          beta.m <- ifelse(random_features==TRUE,sample(1:15,1),supress_features[w,"beta.v"] %>% as.vector() %>% unlist() %>% unname())
+            }else{
+        if(random_features==TRUE){
+          beta.m <- sample(beta.val,1)
+            }else{
+          beta.m <- supress_features[w,"beta.v"] %>% as.vector() %>% unlist() %>% unname()
+          }
+        }
     
     argsMX=c(
-      "autofeature=false", # Prevent the creation of automatic features or relatiohips between predictors provided to the model and the response variable
-      xFeature, # Fearures to be included into the model creation
-      "defaultprevalence=1.00",
-      paste0("betamultiplier=",beta.m), # random beta multiplier set between 1-15, if random_features==FALSE all possible multipliers are applied to each combination of autofeatures (beta.v is created in line 519
-      "pictures=false",
-      # "biasfile" <- need to try this again at some point! 
-      paste0("replicates=",1), # number of replicates for each model
-      "writeplotdata=true",
-      paste0("threads=",detectCores(logical=TRUE)-2), # NEW
-      "hinge=true",
-      paste0("randomtestpoints=",100-(100-Test_n)), # NEW percentage of points to run out of sample AUC values 
-      "writeplotdata=true", 
-      paste0('threads=',ifelse((detectCores(logical=FALSE)-2)>1,detectCores(logical=FALSE)-2,1)), # Numeric. The number of processor threads to use. 
-      # Matching this number to the number of cores on your
-      # computer speeds up some operations, especially variable jackknifing.
-      "responsecurves=false",
-      # "jackknife=false", #Logical if TRUE measures the importance of each environmental variable by training 
-      # with each environmental variable first omitted, then used in isolation
-      "askoverwrite=false"
-      # Check the parameters to write the data into a designated folder
-    )
+          "autofeature=false", # Prevent the creation of automatic features or relationships between predictors provided to the model and the response variable
+          xFeature, # Features to be included in the model
+          "defaultprevalence=1.00",
+          paste0("betamultiplier=",beta.m), # random beta multiplier set between 1-15, if random_features==FALSE all possible multipliers are applied to each combination of autofeatures (beta.v is created in line 519
+          "pictures=false",
+          # "biasfile" <- need to try this again at some point! 
+          paste0("replicates=",1), # number of replicates for each model
+          "writeplotdata=true",
+          paste0("threads=",detectCores(logical=TRUE)-2), # NEW
+          "hinge=true",
+          paste0("randomtestpoints=",100-(100-Test_n)), # NEW percentage of points to run out of sample AUC values 
+          "writeplotdata=true", 
+          paste0('threads=',ifelse((detectCores(logical=FALSE)-2)>1,detectCores(logical=FALSE)-2,1)), # Numeric. The number of processor threads to use. 
+          # Matching this number to the number of cores on your
+          # computer speeds up some operations, especially variable jackknifing.
+          "responsecurves=false",
+          # "jackknife=false", #Logical if TRUE measures the importance of each environmental variable by training 
+          # with each environmental variable first omitted, then used in isolation
+          "askoverwrite=false"
+          # Check the parameters to write the data into a designated folder
+          )
     
     # 4.1.b Run the model ----
     if(is.null(Mod.route)){
@@ -360,7 +415,6 @@ Auto_maxent<-function(
     TSS.mean.TEST <- y.acc[rownames(y.acc) %in% "TSS",] %>% mean(na.rm=TRUE)
     
     if(length(TSS.threshold.TEST)==0){TSS.threshold.TEST <- "no.data"}
-    
     
     # 5.2 Get model thresholds----
     # MaxEnt
@@ -571,7 +625,13 @@ Auto_maxent<-function(
     # Reduce the data to macth the model selection
     models <- models[names(models) %in% select_models]
     mod.params <- mod.params %>% filter(model.id %in% select_models) 
-    AICc_mods <- AICc_mods %>% filter(mod %in% select_models) 
+    
+    if(mod.select==TRUE){
+      AICc_mods <- AICc_mods %>% filter(mod %in% select_models)
+        }else{
+      AICc_mods <- NULL
+        }
+    
     mod.ARGS <- mod.ARGS[names(mod.ARGS) %in% select_models]
     
       # 7. Prepare the return object ----
