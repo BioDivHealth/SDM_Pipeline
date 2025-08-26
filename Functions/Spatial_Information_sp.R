81ab25b90fbb21aafd77a6ddcf0775db2e086f0e
GonzaloAlbaladejo <gonalbala@gmail.com>
2025-08-26 11:35:54 +0100

    Update Spatial_Information_sp.R
    Add a timer to the GBIF download function to avoid API restrictions

--- Functions/Spatial_Information_sp.R
+++ Functions/Spatial_Information_sp.R
@@ -548,8 +548,8 @@ Download_gbif<-function(sp_list, # (Character) List of species from which to dow
         
         points <- NULL
         t_11 <- 1
-        while(is.null(points) && t_11 <= 1000) {
-          
+        while(is.null(points) && t_11 <= 100) {
+          Sys.sleep(1)
           points<-try(occ_search(scientificName =sp_list,
                                  hasCoordinate=TRUE,
                                  year=years[k],
@@ -605,8 +605,8 @@ Download_gbif<-function(sp_list, # (Character) List of species from which to dow
           points <- NULL
           t_11 <- 1
           
-          while(is.null(points) && t_11 <= 1000) {
-            
+          while(is.null(points) && t_11 <= 100) {
+            Sys.sleep(1)
             points<-try(occ_search(scientificName =sp,
                                    hasCoordinate=TRUE,
                                    year=years[k],
